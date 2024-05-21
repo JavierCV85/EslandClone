21e1e7db097746796fbcc1cc1231513cbca9893f
JavierCV85 <47248468+JavierCV85@users.noreply.github.com>
2024-05-21 16:58:55 +0200

    Add preact & numbers component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), preact()],
   i18n: {
     defaultLocale: 'es',
-    locales: ['es','ca'],
+    locales: ['es', 'ca'],
     routing: {
       prefixDefaultLocale: false
     }
